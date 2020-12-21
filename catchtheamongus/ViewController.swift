4c7fc55e8707dbd792ee675235c2020f29452f95
KaanKIVIRCIK <kaankvrck@icloud.com>
2020-12-21 14:29:30 +0300

    Initial Commit

---
+++ catchtheamongus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catchtheamongus
+//
+//  Created by Kaan Kıvırcık on 21.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
